5a392ee1783c2b4b6b129f96eb3c60f9ae406034
cosmicpb <pbaldacimjr@gmail.com>
2022-11-01 17:23:08 -0300

    Update create_graph.py

--- create_graph.py
+++ create_graph.py
@@ -5,93 +5,95 @@ import matplotlib.pyplot as plt
 import csv
 
 ##CREATING SORTED RANDOM ARRAY
-bolso = np.random.uniform(low=40.0, high=55.0, size=(1000,))
-lula = np.random.uniform(low=45.0, high=60.0, size=(1000,))
+cand1 = np.random.uniform(low=40.0, high=55.0, size=(1000,))
+cand2 = np.random.uniform(low=45.0, high=60.0, size=(1000,))
 
-bolso[::-1].sort()
-lula.sort()
+cand1[::-1].sort()
+cand2.sort()
 
 ##MOVING TO EVEN INDEXES
-bolsoT = np.zeros(2000)
+cand1T = np.zeros(2000)
 
 cont = 0
-for line in bolso:
-    bolsoT[cont*2] = float(line)
+for line in cand1:
+    cand1T[cont*2] = float(line)
     cont = cont + 1
 
-lulaT = np.zeros(2000)
+cand2T = np.zeros(2000)
 
 cont = 0
-for line in lula:
+for line in cand2:
 
-    lulaT[cont*2] = float(line)
+    cand2T[cont*2] = float(line)
     cont = cont + 1
 
 ##CREATING UNIFORM RANDOM ARRAY
-bolsoD = np.random.uniform(low=40.0, high=60.0, size=(1000,))
-lulaD = np.random.uniform(low=40.0, high=60.0, size=(1000,))
+cand1D = np.random.uniform(low=40.0, high=60.0, size=(1000,))
+cand2D = np.random.uniform(low=40.0, high=60.0, size=(1000,))
 
 #MOVING TO ODD INDEXES
 cont = 0
-for line in bolsoD:
-    bolsoT[cont*2-1] = float(line)
+for line in cand1D:
+    cand1T[cont*2-1] = float(line)
     cont = cont + 1
 
 cont = 0
-for line in lulaD:
+for line in cand2D:
 
-    lulaT[cont*2-1] = float(line)
+    cand2T[cont*2-1] = float(line)
     cont = cont + 1
 
 
-bolsoS = np.zeros(2002)
+cand1S = np.zeros(2002)
 
 
 cont2 = 0
-for line in bolsoT:
-    bolsoS[cont2+1] = bolsoS[cont2] + line
+for line in cand1T:
+    cand1S[cont2+1] = cand1S[cont2] + line
     cont2 = cont2 + 1
-print(bolsoS)
+print(cand1S)
 
-candBolso = np.zeros(2002)
+candcand1 = np.zeros(2002)
 cont = 0
-for line in bolsoS:
-    candBolso[cont] = line/(cont+1)
+for line in cand1S:
+    candcand1[cont] = line/(cont+1)
     cont = cont + 1
 
 candFinal = np.zeros(2000)
 cont = 0
 for line in candFinal:
-    candFinal[cont] = candBolso[cont+1]
+    candFinal[cont] = candcand1[cont+1]
     cont = cont + 1
 
     
 print(candFinal)
 
 #CREATING AGGREGATE AVERAGE ARRAY
-lulaS = np.zeros(2002)
+cand2S = np.zeros(2002)
 cont2 = 0
-for line in lulaT:
-    lulaS[cont2+1] = lulaS[cont2] + line
+for line in cand2T:
+    cand2S[cont2+1] = cand2S[cont2] + line
     cont2 = cont2 + 1
-print(lulaS)
+print(cand2S)
 
-lulaBolso = np.zeros(2002)
+cand2cand1 = np.zeros(2002)
 cont = 0
-for line in lulaS:
-    lulaBolso[cont] = line/(cont+1)
+for line in cand2S:
+    cand2cand1[cont] = line/(cont+1)
     cont = cont + 1
 
-lulaFinal = np.zeros(2000)
+cand2Final = np.zeros(2000)
 cont = 0
-for line in lulaFinal:
-    lulaFinal[cont] = lulaBolso[cont+1]
+for line in cand2Final:
+    cand2Final[cont] = cand2cand1[cont+1]
     cont = cont + 1
 
 
 x = np.arange(0, 2000)
 print(x[1999])
 
-plt.plot(x, lulaFinal, color='red')
-plt.plot(x, candFinal, color='blue')
+plt.plot(x, candFinal, color='blue', label='Candidato 1')
+plt.plot(x, cand2Final, color='red', label='Candidato 2')
+
+plt.legend()
 plt.show()
